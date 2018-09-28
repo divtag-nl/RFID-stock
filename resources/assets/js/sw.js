c081e0c0e6b6d0ea6431f8bbf06f0b9a47abe5af
Luuk Hoeben <info@luukhoeben.nl>
2018-09-28 11:42:03 +0200

    sw cleanup

--- resources/assets/js/sw.js
+++ resources/assets/js/sw.js
@@ -1,24 +1,15 @@
-var cacheName = '23094568';
+var cacheName = 'rfid-stock-v1';
 var contentToCache = [];
 
-self.addEventListener('install', function(e) {
+self.addEventListener('install', function (e) {
     console.log('[Service Worker] Installed!');
-
-    e.waitUntil(
-        caches.open(cacheName).then(function(cache) {
-            console.log('[Service Worker] Caching all: app shell and content');
-            return cache.addAll(contentToCache);
-        })
-    );
 });
 
-self.addEventListener('activate', function(e) {
-    console.log('test activate');
+self.addEventListener('activate', function (e) {
     e.waitUntil(
         caches.keys().then(function(keyList) {
-            console.log(keyList);
-            return Promise.all(keyList.map(function(key) {
-                if(cacheName.indexOf(key) === -1) {
+            return Promise.all(keyList.map(function (key) {
+                if (cacheName.indexOf(key) === -1) {
                     return caches.delete(key);
                 }
             }));
@@ -26,27 +17,30 @@ self.addEventListener('activate', function(e) {
     );
 });
 
-self.addEventListener('fetch', function(e) {
+self.addEventListener('fetch', function (e) {
     console.log('[Service Worker] Fetching resource: ' + e.request.url);
 
     if (navigator.onLine) {
         e.respondWith(
-            caches.match(e.request).then(function(r) {
-                console.log('[Service Worker] Fetching resource: '+e.request.url);
-                return fetch(e.request).then(function(response) {
-                    return caches.open(cacheName).then(function(cache) {
-                        console.log('[Service Worker] Caching new resource: '+e.request.url);
+            caches.match(e.request).then(function (r) {
+                console.log('[Service Worker] Fetching resource: ' + e.request.url);
+
+                return fetch(e.request).then(function (response) {
+                    return caches.open(cacheName).then(function (cache) {
+                        console.log('[Service Worker] Caching new resource: ' + e.request.url);
+
                         cache.put(e.request, response.clone());
+
                         return response;
                     });
                 });
             })
         );
     } else {
-        // get from cache...
         e.respondWith(
             caches.match(e.request).then(function(r) {
                 console.log('[Service Worker] Fetching resource: '+e.request.url);
+
                 return r;
             })
         );
